4e03c48d29e882caf3dfd0479f5e47042ae58392
oneillm <oneillm@mail.sfsu.edu>
2014-06-22 12:45:08 -0700

    Modified business listing before bootstrap

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,8 +3,6 @@ class Business < ActiveRecord::Base
     
    def self.search(search)
       if search
-         #any_of({bname: /#{search}/i}, {bphone: /#{search}/i})
-      #:conditions => ['LOWER(family_name) like ? OR LOWER (given_name LIKE ?', "%#{params[:search].downcase}%","%#{params[:search].downcase}%"]   
          find(:all, :conditions => ['LOWER(bname) like ? OR ? = ANY(bphone)', "%#{search.downcase}%","#{search}"]) 
      else
          self.all
