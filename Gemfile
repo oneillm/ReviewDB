249ebf01c41fd79cee6324507b03839614ec8f19
oneillm <oneillm@mail.sfsu.edu>
2014-06-24 00:27:43 -0700

    reapply css to business index page

--- Gemfile
+++ Gemfile
@@ -32,6 +32,10 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+#group :assets do
+   gem 'twitter-bootstrap-rails'
+#end
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  session :session_key => 'new_maintenance_admin'
-  helper_method :newadmin:
+#  session :session_key => 'new_maintenance_admin'
+  helper_method :newadmin?
   protected
 def configure_permitted_parameters
  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit! }
  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password)}
 end
 def authorize
-   unless newadmin:
-     flash[:error} = "unauthorized access"
+   unless newadmin?
+     flash[:error] = "unauthorized access"
      redirect_to home_path
      false
    end
