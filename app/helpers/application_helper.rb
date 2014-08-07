93bc190db22cd36b291eb8d726ff5556680680bf
oneillm <ruby88@gmail.com>
2014-08-06 21:00:54 -0700

    added placeholder and tooltips css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,5 +28,11 @@ module ApplicationHelper
                    flash_type.to_s
             end
         end
+       def tooltip_for(text, &block)
+    	content = capture(&block)
+    	content_tag :div, class: "tooltip_wrapper" do
+      	content + content_tag(:div, content_tag(:div, "", class: "tooltip_triangle") + text, class: "helper_tooltip")
+       end
+  end
 
 end
