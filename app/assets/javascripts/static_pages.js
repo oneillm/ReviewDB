e32961eb1a3817581b5e9885dd591dc7693b89e2
Ubuntu <ubuntu@ip-172-31-14-236.us-west-1.compute.internal>
2014-08-23 21:46:00 +0000

    delete static_pages.js commentlines with#,should be using //(javascript command sign)

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,11 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-# called from a bootstrap dropdown, this closes the dropdown
 
  jQuery(document).ready(function($){
  $('#basicModal').modal(options)
    var body = $( 'show');
  });
-
-#$('#basicModal').modal('show')
